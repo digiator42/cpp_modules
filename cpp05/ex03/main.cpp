75dfeceeb168b8bad39b39e76aa4b411b59f3eeb
Ahmed Hassan <digiator2020@gmail.com>
2023-06-28 23:41:51 +0400

    proper exception msg

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -7,22 +7,29 @@
 
 int main() {
     
+    try
+    {
+        Bureaucrat bureaucrat("Madam Samah", 45);
+
+        Intern intern;
+        AForm *found;
+        AForm *notFound;
+        found = intern.makeForm("ShrubberyCreationForm", "FOUND");
+
+
+        // found->beSigned(bureaucrat);
+        // found->execute(bureaucrat);
+
+        std::cout << found->getName() + '\n';
+        
+        notFound = intern.makeForm("NOT-VALID", "NV"); //exception
+        // notFound->beSigned(bureaucrat);
+        // notFound->execute(bureaucrat);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << '\n';
+    }
     
-    Bureaucrat bureaucrat("Madam Samah", 45);
-
-    Intern intern;
-    AForm *found;
-    // AForm *notFound;
-    found = intern.makeForm("ShrubberyCreationForm", "FOUND");
-
-
-    found->beSigned(bureaucrat);
-    found->execute(bureaucrat);
-
-    std::cout << found->getName() + '\n';
-    
-    // notFound = intern.makeForm("NOT-VALID", "NV"); //exception
-    // notFound->beSigned(bureaucrat);
-    // notFound->execute(bureaucrat);
     return 0;
 }
