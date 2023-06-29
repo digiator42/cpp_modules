b3a641a35875a5d25268995bda578020494ef67a
Ahmed Hassan <digiator2020@gmail.com>
2023-06-29 17:01:33 +0400

    rm Form

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include "Bureaucrat.hpp"
-#include "Form.hpp"
 
 int main() {
     try {
         Bureaucrat bureaucrat("Madam Samah", 50);
         std::cout << bureaucrat << std::endl;
 
-        Form form("Form 1", 60, 70);
-        std::cout << form << std::endl;
+        // Form form("Form 1", 60, 70);
+        // std::cout << form << std::endl;
 
         try {
-            form.beSigned(bureaucrat);
-            std::cout << form << std::endl;
+            // form.beSigned(bureaucrat);
+            // std::cout << form << std::endl;
         } catch (const std::exception& e) {
             std::cout << "Exception: " << e.what() << std::endl;
         }
@@ -21,8 +20,8 @@ int main() {
         std::cout << highGradeBureaucrat << std::endl;
 
         try {
-            form.beSigned(highGradeBureaucrat);
-            std::cout << form << std::endl;
+            // form.beSigned(highGradeBureaucrat);
+            // std::cout << form << std::endl;
         } catch (const std::exception& e) {
             std::cout << "Exception: " << e.what() << std::endl;
         }
