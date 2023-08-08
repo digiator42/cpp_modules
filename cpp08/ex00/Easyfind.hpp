1ab8c7867245499676e1429d56ded88dbe3ab622
Ahmed Hassan <digiator2020@gmail.com>
2023-08-08 23:36:11 +0400

    containers

--- cpp08/ex00/Easyfind.hpp
+++ cpp08/ex00/Easyfind.hpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <map>
+#include <list>
+#include <set>
 
 
+// find element in container
 template<typename Container, typename T>
 typename Container::iterator easyFind(Container &container, const T &toFind) {
     for (typename Container::iterator it = container.begin(); it != container.end(); ++it) {
@@ -10,3 +14,28 @@ typename Container::iterator easyFind(Container &container, const T &toFind) {
     }
     throw std::runtime_error("couldn't find element");
 }
+
+// find element in map
+template<typename Key, typename Value>
+typename std::map<Key, Value>::iterator easyFind(std::map<Key, Value> &container, const Key &toFind) {
+    typename std::map<Key, Value>::iterator it = container.begin();
+    for (; it != container.end(); ++it) {
+        if (it->first == toFind) {
+            return it;
+        }
+    }
+    throw std::runtime_error("couldn't find element");
+}
+
+// find element in container
+template<typename Key, typename Value>
+typename std::set<int>::iterator easyFind(std::set<int> &container, const Key &toFind) {
+    typename std::set<int>::iterator it = container.begin();
+    for (; it != container.end(); ++it) {
+        if (*it == toFind) {
+            return it;
+        }
+    }
+    throw std::runtime_error("couldn't find element");
+}
+
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,16 +1,35 @@
 #include "Easyfind.hpp"
 #include <vector>
-
 int main() {
-	std::vector<int> array;
+
+	std::vector<int> vector;
+	std::list<int> list;
+	std::set<int> set;
+	std::map<int, std::string> map;
 
 	int i = 0;
-	while (i < 10)
-		array.push_back(i++);
+	while (i < 10) {
+		vector.push_back(i);
+		list.push_front(vector[i]);
+		set.insert(vector[i++]);
+		map.insert(std::make_pair(i, "hello " + std::to_string(i)));
+	}
 
 	try {
-		std::vector<int>::iterator it = easyFind(array, i);
-		std::cout << *it << std::endl;
+
+		std::vector<int>::iterator v = easyFind(vector, --i);
+		std::cout << *v << std::endl;
+
+		std::list<int>::iterator l = easyFind(list, i);
+		std::cout << *l << std::endl;
+		
+		std::set<int>::iterator seit = easyFind(set, i);
+		std::cout << *seit << std::endl;
+
+
+		std::map<int, std::string>::iterator s = easyFind(map, i);
+		std::cout << s->first << " " << s->second << std::endl;
+
 	} catch(std::exception &e) {
 		std::cout << "Exception: " << e.what() << std::endl;
 	}
