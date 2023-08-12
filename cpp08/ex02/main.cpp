747d688ed9a98a09d505d37893f144b1b5199d41
Ahmed Hassan <digiator2020@gmail.com>
2023-08-12 17:59:46 +0400

    tried with list

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,6 +1,8 @@
 #include "MutantStack.hpp"
+#include <list>
 
 int main () {
+
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
@@ -11,8 +13,34 @@ int main () {
     mstack.push(5);
     mstack.push(737);
     mstack.push(0);
-    MutantStack<int>::iterator itb = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
+    MutantStack<int>::iterator itl = mstack.begin();
+    MutantStack<int>::iterator itel = mstack.end();
+    ++itl;
+    --itl;
+    std::cout << "stack: ";
+    while (itl != itel) {
+        std::cout << *itl << " ";
+        ++itl;
+    }
+    std::cout << std::endl;
+    while (!mstack.empty()) {
+        std::cout << mstack.top() <<" "; 
+        mstack.pop();
+    }
+    std::cout << "\n----------list-----------" << std::endl;
+    
+    std::list<int> lit;
+    lit.push_back(5);
+    lit.push_back(17);
+    std::cout << "top: " << lit.front() << std::endl;
+    lit.remove(17);
+    std::cout << "size: " << lit.size() << std::endl;
+    lit.push_back(3);
+    lit.push_back(5);
+    lit.push_back(737);
+    lit.push_back(0);
+    std::list<int>::iterator itb = lit.begin();
+    std::list<int>::iterator ite = lit.end();
     ++itb;
     --itb;
     std::cout << "stack: ";
