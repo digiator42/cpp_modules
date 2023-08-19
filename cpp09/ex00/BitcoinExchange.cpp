ac1cefcc81ccc198a6dfba35663426f1ef236411
Ahmed Hassan <digiator2020@gmail.com>
2023-08-19 23:15:16 +0400

    feb parse

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -73,6 +73,8 @@ static bool isValidDate(const std::string& dateStr) {
     if (month < 1 || month > 12 || day < 1 || day > 31) {
         return false;
     }
+    if (month == 2 && day > 29)
+        return false;
 
     if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
         return false;
