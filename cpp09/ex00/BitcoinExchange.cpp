ca4f6b5e59dd2c6a520879e21afc0323b5c3206f
Ahmed Hassan <109759977+digiator42@users.noreply.github.com>
2023-08-20 01:53:14 +0400

    map value

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -39,7 +39,7 @@ BitcoinExchange::BitcoinExchange(char *filePath) {
         vec = split(std::string(buffer), ',');
         if (vec.size() != 2)
             throw exception("Wrong database.");
-		this->map.insert(std::make_pair(vec.at(0), std::atof(vec.at(0).c_str())));
+		this->map.insert(std::make_pair(vec.at(0), std::atof(vec.at(1).c_str())));
     }
 }
 
