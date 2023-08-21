f19dce584553e5a4511c292c63e5bc4df10e869d
Ahmed Hassan <digiator2020@gmail.com>
2023-08-21 05:16:37 +0400

    error msg, multiple spaces err

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -68,6 +68,13 @@ void    BitcoinExchange::printMap(void) {
    std::cout << "Size -->: " << map.size() << std::endl;
 }
 
+static std::string to_string(float value) {
+    std::stringstream ss;
+	ss << value;
+	std::string s = ss.str();
+	return s;
+}
+
 static bool isValidDate(const std::string& dateStr) {
     std::istringstream dateStream(dateStr);
     int year, month, day;
@@ -75,7 +82,8 @@ static bool isValidDate(const std::string& dateStr) {
     std::string invalid = "";
 
     dateStream >> year >> dash1 >> month >> dash2 >> day >> invalid;
-    
+    if (to_string(year).length() != 4)
+        return false;
     if (!invalid.empty() || dash1 != '-' || dash2 != '-') {
         return false;
     }
@@ -101,72 +109,72 @@ void    BitcoinExchange::displayData() {
 
     std::fstream fin(this->filePath);
     if (!fin.getline(buffer, 1024)) 
-        throw exception("Input file is empty.");
+        throw exception("Error: Input file is empty.");
 
     if (strcmp(buffer, "date | value") != 0)
-		throw exception("Invalid header: " + std::string(buffer));
+		throw exception("Error: Invalid header: " + std::string(buffer));
 
     while(fin.getline(buffer, 1024))
     {
         std::string buff(buffer);
         if (buff.find("|") != std::string::npos)
             if (buff.substr(buff.find("|") + 1, buff.length() - 1).find("|") != std::string::npos) {
-                std::cerr << "Bad DOUBLE input: " << buff << std::endl;
+                std::cerr << "Error: Bad DOUBLE input: " << buff << std::endl;
                 continue;
         }
         if (std::isspace(buff[0]) || std::isspace(buff[buff.length() - 1])) {
-                std::cerr << "Bad SPACE input: " << buff << std::endl;
+                std::cerr << "Error: Bad SPACE input: " << buff << std::endl;
                 continue;
         }                
         if (buff.find("|") == std::string::npos) {
-            std::cerr << "Bad NO PIPE input: " << buff << std::endl;
+            std::cerr << "Error: Bad NO PIPE input: " << buff << std::endl;
             continue;
         }
         if (buff.find_first_not_of("0123456789-| .") != std::string::npos) {
-            std::cerr << "Bad input: " << buff << std::endl;
+            std::cerr << "Error: Bad input: " << buff << std::endl;
             continue;
         }
-        if (buff.length() >= 13 && buff.substr(10, 3).compare(" | ") != 0) {
-            std::cerr << "Bad INDENT input: " << buff << std::endl;
-            continue;
-        }
+
         vec = split(buffer, '|');
 
         if (vec.size() != 2) {
-            std::cerr << "Bad vector input: " << buff << std::endl;
-            continue;
-        }
-        if (!isValidDate(vec.at(0))) {
-            std::cerr << "invalid date format: " << buff << std::endl;
+            std::cerr << "Error: Bad vector input: " << buff << std::endl;
             continue;
         }
 
         float               Digit;
         std::stringstream   ss_digit(vec.at(1));
         ss_digit >> Digit;
+        int diff = buff.length() - 10 - to_string(Digit).length();
 
-        if (!strptime(vec.at(0).c_str(), "%Y-%m-%d", &time)) {
-            std::cerr << "Error: bad strptime input => " << vec.at(0) <<std::endl;
-            continue;
-        }
         if (Digit < 0) {
                 std::cerr << "Error: negative num " << vec.at(1) << std::endl;
                 continue;
         }
         if (Digit > 1000) {
-                std::cerr << "Error: over max num " << vec.at(1) << std::endl;
+                std::cerr << "Error: too large number " << vec.at(1) << std::endl;
                 continue;
+        }
+        if (buff.length() >= 13 && buff.substr(10, diff).compare(" | ") != 0) {
+            std::cerr << "Error: Bad INDENT input: " << buff << std::endl;
+            continue;
+        }
+
+        if (!isValidDate(vec.at(0))) {
+            std::cerr << "Error: invalid date format: " << buff << std::endl;
+            continue;
+        }
+
+        if (!strptime(vec.at(0).c_str(), "%Y-%m-%d", &time)) {
+            std::cerr << "Error: bad strptime input => " << vec.at(0) <<std::endl;
+            continue;
         }
 
         std::map <std::string, float>::iterator itr;
 
-        itr = this->map.find(vec.at(0));
-        if (itr == this->map.end()) {
-            itr = this->map.lower_bound(vec.at(0));
-            if (itr != map.begin())
+        itr = this->map.lower_bound(vec.at(0));
+        if (itr != map.begin() || itr == this->map.end())
                 itr--;
-        }
-
         float output = (*itr).second * Digit;
         std::cout << vec.at(0).c_str() << " => " << Digit << " = " << output << std::endl;
     }
